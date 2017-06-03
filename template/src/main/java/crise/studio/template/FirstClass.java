c56b3a319a9aa25aa2a988c32cd7736ff2faa18d
woochoul <urlover@naver.com>
2017-06-03 23:03:59 +0900

    test commit first

---
+++ template/src/main/java/crise/studio/template/FirstClass.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crise.studio.template;
+
+/**
+ *
+ * @author wclee
+ */
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("first github upload");
+        
+        
+    }
+}
